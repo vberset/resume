ba9e0383b8bb0d999901916c2cc68b5e7427ea29
Vincent Berset <vincent.berset@infomaniak.com>
2021-07-19 13:07:01 +0200

    refactor: DRY on report building

--- src/report.rs
+++ src/report.rs
@@ -1,35 +1,42 @@
 use std::fmt::Write;
 
-use crate::error::Result;
-use crate::message::CommitType;
-use crate::ChangeLog;
+use crate::{
+    ChangeLog,
+    error::Result,
+    message::{CommitType, ConventionalMessage},
+};
 
-pub fn build_report(output: &mut dyn Write, changelog: &ChangeLog) -> Result<()> {
-    if let Some(features) = changelog.get(&CommitType::Feature) {
-        writeln!(output, "    ✨ New Features\n")?;
-        for message in features {
-            writeln!(
-                output,
-                "       - {} {}",
-                if message.is_breaking { "💥 " } else { "" },
-                message.summary
-            )?;
+pub fn build_report<W: Write>(output: &mut W, changelog: &ChangeLog) -> Result<()> {
+    let categories = [
+        (CommitType::Feature, '✨', "New Features"),
+        (CommitType::BugFix, '🐛', "Bug Fixes"),
+        (CommitType::Refactoring, '♻', "Refactoring"),
+    ];
+
+    for category in categories {
+        if let Some(messages) = changelog.get(&category.0) {
+            format_category(output, category.1, category.2, messages)?;
         }
-        writeln!(output)?;
     }
 
-    if let Some(features) = changelog.get(&CommitType::BugFix) {
-        writeln!(output, "    🐛 Bug Fixes\n")?;
-        for message in features {
-            writeln!(
-                output,
-                "       - {} {}",
-                if message.is_breaking { "💥 " } else { "" },
-                message.summary
-            )?;
-        }
-        writeln!(output)?;
-    }
+    Ok(())
+}
 
+fn format_category<W: Write>(
+    output: &mut W,
+    emoji: char,
+    title: &str,
+    messages: &[ConventionalMessage],
+) -> Result<()> {
+    writeln!(output, "    {}️ {}\n", emoji, title)?;
+    for message in messages {
+        writeln!(
+            output,
+            "       - {} {}",
+            if message.is_breaking { "💥 " } else { "" },
+            message.summary
+        )?;
+    }
+    writeln!(output)?;
     Ok(())
 }
